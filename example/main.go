89ae50642dc8b0d667173b4f729e3ad54341056a
Eason Lin <easonlin404@gmail.com>
2017-06-10 18:32:27 +0800

    fmt

--- example/main.go
+++ example/main.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
-	"github.com/easonlin404/gin-swagger"
 	"fmt"
+	"github.com/easonlin404/gin-swagger"
+	"github.com/gin-gonic/gin"
 )
 
-func main(){
-	router:= gin.New()
+func main() {
+	router := gin.New()
 	router.GET("/favicon.ico", handlerTest1)
 	router.GET("/", handlerTest1)
 	group := router.Group("/users")
@@ -17,18 +17,15 @@ func main(){
 		group.POST("/:id", handlerTest2)
 	}
 
-	swagg:= swagger.New(router.Routes())
+	swagg := swagger.New(router.Routes())
 
 	swagg.Build()
 
 	fmt.Println(swagg.Routes())
 
-
 	router.Run()
 
-
 }
-
 
 func handlerTest1(c *gin.Context) {}
 func handlerTest2(c *gin.Context) {}
