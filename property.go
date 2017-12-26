f982e19670b4e43d7d1bb8871da5239692bd1049
Wei Huang <wei.huang@C02NTFYYG3QD.Corp.ad.ctc>
2017-12-25 23:23:10 -0500

    adding type to support the Bson ObjectId and fix the int for goswagger only support integer type

--- property.go
+++ property.go
@@ -10,8 +10,14 @@ import (
 func getPropertyName(field *ast.Field) string {
 	var name string
 	if astTypeSelectorExpr, ok := field.Type.(*ast.SelectorExpr); ok {
+
 		// Support for time.Time as a structure field
 		if "Time" == astTypeSelectorExpr.Sel.Name {
+			return "string"
+		}
+
+		// Support bson.ObjectId type
+		if "ObjectId" == astTypeSelectorExpr.Sel.Name {
 			return "string"
 		}
 
@@ -19,6 +25,12 @@ func getPropertyName(field *ast.Field) string {
 
 	} else if astTypeIdent, ok := field.Type.(*ast.Ident); ok {
 		name = astTypeIdent.Name
+
+		// When its the int type will transfer to integer which is goswagger supported type
+		if "int" == name {
+			return "integer"
+		}
+
 	} else if _, ok := field.Type.(*ast.StarExpr); ok {
 		panic("not supported astStarExpr yet.")
 	} else if _, ok := field.Type.(*ast.MapType); ok { // if map
