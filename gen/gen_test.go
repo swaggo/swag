7f7eeb3a2586c76009cd0c83b8ab2a6ec8be7a88
pei0804 <peeeei0804@gmail.com>
2018-03-13 16:55:46 +0900

    test data move to testdata dir

--- gen/gen_test.go
+++ gen/gen_test.go
@@ -9,18 +9,18 @@ import (
 )
 
 func TestGen_Build(t *testing.T) {
-	searchDir := "../example/simple"
+	searchDir := "../testdata/simple"
 	assert.NotPanics(t, func() {
-		New().Build(searchDir, "./main.go", "../example/simple/docs/swagger")
+		New().Build(searchDir, "./main.go", "../testdata/simple/docs/swagger")
 	})
 
 	if _, err := os.Stat(path.Join(searchDir, "docs", "docs.go")); os.IsNotExist(err) {
 		t.Fatal(err)
 	}
-	if _, err := os.Stat(path.Join("../example/simple/docs/swagger", "swagger.json")); os.IsNotExist(err) {
+	if _, err := os.Stat(path.Join("../testdata/simple/docs/swagger", "swagger.json")); os.IsNotExist(err) {
 		t.Fatal(err)
 	}
-	if _, err := os.Stat(path.Join("../example/simple/docs/swagger", "swagger.yaml")); os.IsNotExist(err) {
+	if _, err := os.Stat(path.Join("../testdata/simple/docs/swagger", "swagger.yaml")); os.IsNotExist(err) {
 		t.Fatal(err)
 	}
 }
