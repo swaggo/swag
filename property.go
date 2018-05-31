db3460d5b0d5eb8cdb90b896d85df0ebd0cee938
pei0804 <peeeei0804@gmail.com>
2018-06-01 08:55:22 +0900

    Revert "refactor: refactoring property.go more simple (#130)"
    This reverts commit 657cedde2f1bfac42ea44b147538bfdaf9503860.

--- property.go
+++ property.go
@@ -11,44 +11,30 @@ type propertyName struct {
 	ArrayType  string
 }
 
-func newSchemaPropertyName(schemaType string) propertyName {
-	return propertyName{
-		SchemaType: schemaType,
-		ArrayType:  "",
-	}
-}
-
-func newArraySchemaPropertyName(arrayType string) propertyName {
-	return propertyName{
-		SchemaType: "array",
-		ArrayType:  arrayType,
-	}
-}
-
 func parseFieldSelectorExpr(astTypeSelectorExpr *ast.SelectorExpr) propertyName {
 	// TODO: In the future, add functions and make them solve for each user
 	// Support for time.Time as a structure field
 	if "Time" == astTypeSelectorExpr.Sel.Name {
-		return newSchemaPropertyName("string")
+		return propertyName{SchemaType: "string", ArrayType: "string"}
 	}
 
 	// Support bson.ObjectId type
 	if "ObjectId" == astTypeSelectorExpr.Sel.Name {
-		return newSchemaPropertyName("string")
+		return propertyName{SchemaType: "string", ArrayType: "string"}
 	}
 
 	// Supprt UUID
 	if "UUID" == strings.ToUpper(astTypeSelectorExpr.Sel.Name) {
-		return newSchemaPropertyName("string")
+		return propertyName{SchemaType: "string", ArrayType: "string"}
 	}
 
 	// Supprt shopspring/decimal
 	if "Decimal" == astTypeSelectorExpr.Sel.Name {
-		return newSchemaPropertyName("number")
+		return propertyName{SchemaType: "number", ArrayType: "string"}
 	}
 
 	fmt.Printf("%s is not supported. but it will be set with string temporary. Please report any problems.", astTypeSelectorExpr.Sel.Name)
-	return newSchemaPropertyName("string")
+	return propertyName{SchemaType: "string", ArrayType: "string"}
 }
 
 // getPropertyName returns the string value for the given field if it exists, otherwise it panics.
@@ -60,7 +46,7 @@ func getPropertyName(field *ast.Field) propertyName {
 	if astTypeIdent, ok := field.Type.(*ast.Ident); ok {
 		name := astTypeIdent.Name
 		schemeType := TransToValidSchemeType(name)
-		return newSchemaPropertyName(schemeType)
+		return propertyName{SchemaType: schemeType, ArrayType: schemeType}
 	}
 	if ptr, ok := field.Type.(*ast.StarExpr); ok {
 		if astTypeSelectorExpr, ok := ptr.X.(*ast.SelectorExpr); ok {
@@ -69,34 +55,34 @@ func getPropertyName(field *ast.Field) propertyName {
 		if astTypeIdent, ok := ptr.X.(*ast.Ident); ok {
 			name := astTypeIdent.Name
 			schemeType := TransToValidSchemeType(name)
-			return newSchemaPropertyName(schemeType)
+			return propertyName{SchemaType: schemeType, ArrayType: schemeType}
 		}
 		if astTypeArray, ok := ptr.X.(*ast.ArrayType); ok { // if array
 			if astTypeArrayIdent := astTypeArray.Elt.(*ast.Ident); ok {
-				arrayType := astTypeArrayIdent.Name
-				return newArraySchemaPropertyName(arrayType)
+				name := astTypeArrayIdent.Name
+				return propertyName{SchemaType: "array", ArrayType: name}
 			}
 		}
 	}
 	if astTypeArray, ok := field.Type.(*ast.ArrayType); ok { // if array
 		if astTypeArrayExpr, ok := astTypeArray.Elt.(*ast.StarExpr); ok {
 			if astTypeArrayIdent := astTypeArrayExpr.X.(*ast.Ident); ok {
-				arrayType := astTypeArrayIdent.Name
-				return newArraySchemaPropertyName(arrayType)
+				name := astTypeArrayIdent.Name
+				return propertyName{SchemaType: "array", ArrayType: name}
 			}
 		}
-		arrayType := fmt.Sprintf("%s", astTypeArray.Elt)
-		return newArraySchemaPropertyName(arrayType)
+		str := fmt.Sprintf("%s", astTypeArray.Elt)
+		return propertyName{SchemaType: "array", ArrayType: str}
 	}
 	if _, ok := field.Type.(*ast.MapType); ok { // if map
 		//TODO: support map
-		return newSchemaPropertyName("object")
+		return propertyName{SchemaType: "object", ArrayType: "object"}
 	}
 	if _, ok := field.Type.(*ast.StructType); ok { // if struct
-		return newSchemaPropertyName("object")
+		return propertyName{SchemaType: "object", ArrayType: "object"}
 	}
 	if _, ok := field.Type.(*ast.InterfaceType); ok { // if interface{}
-		return newSchemaPropertyName("object")
+		return propertyName{SchemaType: "object", ArrayType: "object"}
 	}
 	panic("not supported" + fmt.Sprint(field.Type))
 }
