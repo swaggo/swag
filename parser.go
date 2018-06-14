0cbf6ce6d046f953b4462b45127956b5f709b30f
Eason Lin <easonlin404@gmail.com>
2018-06-14 12:26:06 +0800

    move const

--- parser.go
+++ parser.go
@@ -19,6 +19,17 @@ import (
 	"github.com/go-openapi/spec"
 )
 
+const (
+	// CamelCase indicates using CamelCase strategy for struct field.
+	CamelCase = "camelcase"
+
+	// PascalCase indicates using PascalCase strategy for struct field.
+	PascalCase = "pascalcase"
+
+	// SnakeCase indicates using SnakeCase strategy for struct field.
+	SnakeCase = "snakecase"
+)
+
 // Parser implements a parser for Go source files.
 type Parser struct {
 	// swagger represents the root document object for the API specification
