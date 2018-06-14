8d7608b4c5baee2abcb82269395cad7e35e46838
Eason Lin <easonlin404@gmail.com>
2018-06-14 12:07:17 +0800

    fix lint

--- strategy.go
+++ strategy.go
@@ -1,7 +1,12 @@
 package swag
 
 const (
+	//CamelCase indicates using CamelCase strategy for struct field.
 	CamelCase  = "camelcase"
+
+	//PascalCase indicates using PascalCase strategy for struct field.
 	PascalCase = "pascalcase"
+
+	//SnakeCase indicates using SnakeCase strategy for struct field.
 	SnakeCase  = "snakecase"
 )
