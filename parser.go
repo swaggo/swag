c7aaff1d5c07def19ef307827b4eb13086bd2809
Eason Lin <easonlin404@gmail.com>
2018-09-12 18:02:08 +0800

    refactor: enhance error handle

--- parser.go
+++ parser.go
@@ -1,6 +1,7 @@
 package swag
 
 import (
+	"errors"
 	"fmt"
 	"go/ast"
 	goparser "go/parser"
@@ -76,9 +77,11 @@ func New() *Parser {
 }
 
 // ParseAPI parses general api info for gived searchDir and mainAPIFile
-func (parser *Parser) ParseAPI(searchDir string, mainAPIFile string) {
+func (parser *Parser) ParseAPI(searchDir string, mainAPIFile string) error {
 	log.Println("Generate general API Info")
-	parser.getAllGoFileInfo(searchDir)
+	if err := parser.getAllGoFileInfo(searchDir); err != nil {
+		return err
+	}
 	parser.ParseGeneralAPIInfo(path.Join(searchDir, mainAPIFile))
 
 	for _, astFile := range parser.files {
@@ -93,12 +96,11 @@ func (parser *Parser) ParseAPI(searchDir string, mainAPIFile string) {
 }
 
 // ParseGeneralAPIInfo parses general api info for gived mainAPIFile path
-func (parser *Parser) ParseGeneralAPIInfo(mainAPIFile string) {
+func (parser *Parser) ParseGeneralAPIInfo(mainAPIFile string) error {
 	fileSet := token.NewFileSet()
 	fileTree, err := goparser.ParseFile(fileSet, mainAPIFile, nil, goparser.ParseComments)
-
 	if err != nil {
-		log.Panicf("ParseGeneralApiInfo occur error:%+v", err)
+		return errors.New("ParseGeneralApiInfo occurs error: %+v", err)
 	}
 
 	parser.swagger.Swagger = "2.0"
@@ -728,8 +730,8 @@ func defineTypeOfExample(schemaType string, exampleValue string) interface{} {
 }
 
 // GetAllGoFileInfo gets all Go source files information for given searchDir.
-func (parser *Parser) getAllGoFileInfo(searchDir string) {
-	filepath.Walk(searchDir, parser.visit)
+func (parser *Parser) getAllGoFileInfo(searchDir string) error {
+	return filepath.Walk(searchDir, parser.visit)
 }
 
 func (parser *Parser) visit(path string, f os.FileInfo, err error) error {
