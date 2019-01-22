1d21c47672cb54dbe2e38e8c2df9391c0b150903
Windpro <windprog@gmail.com>
2019-01-22 20:43:59 +0800

    fix bug: []map[string]string type will crash!

--- parser.go
+++ parser.go
@@ -541,8 +541,9 @@ func (parser *Parser) parseTypeExpr(pkgName, typeName string, typeExpr ast.Expr)
 	case *ast.Ident:
 		refTypeName := fullTypeName(pkgName, expr.Name)
 		if _, isParsed := parser.swagger.Definitions[refTypeName]; !isParsed {
-			typedef := parser.TypeDefinitions[pkgName][expr.Name]
-			parser.ParseDefinition(pkgName, expr.Name, typedef)
+			if typedef, ok := parser.TypeDefinitions[pkgName][expr.Name]; ok{
+				parser.ParseDefinition(pkgName, expr.Name, typedef)
+			}
 		}
 		return parser.swagger.Definitions[refTypeName]
 
