f33a679f7847cd8c66b728e07f33d255181d4f14
Tobias Theel <tt@fino.digital>
2019-04-16 13:56:48 +0200

    Added new Feature that allows to use markdown files as tag description

--- parser_test.go
+++ parser_test.go
@@ -15,7 +15,7 @@ import (
 
 func TestNew(t *testing.T) {
 	swagMode = test
-	New("")
+	New()
 }
 
 func TestParser_ParseGeneralApiInfo(t *testing.T) {
@@ -2492,7 +2492,7 @@ func TestParseApiMarkdownDescription(t *testing.T) {
 	if err != nil {
 		t.Log("Failed to parse api description: " + err.Error())
 		t.FailNow()
-	}
+    }
 
 	if p.swagger.Info.Description == "" {
 		t.Log("Failed to parse api description: " + err.Error())
