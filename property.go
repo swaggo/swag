c52d7bf06ee5312cee05f643c64845eac919eb8e
Eason Lin <easonlin404@gmail.com>
2019-05-20 17:42:28 +0800

    refactor: return error instead of panic (#399)
    * refactor: return error instead of panic
    
    * fix fmt check
    
    * fix error check
    
    * refine getPropertyName
    
    * refine CheckSchemaType method

--- property.go
+++ property.go
@@ -90,7 +90,6 @@ func getPropertyName(expr ast.Expr, parser *Parser) (propertyName, error) {
 	}
 
 	if _, ok := expr.(*ast.MapType); ok { // if map
-		//TODO: support map
 		return propertyName{SchemaType: "object", ArrayType: "object"}, nil
 	}
 
