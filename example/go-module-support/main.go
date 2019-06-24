7e49ee56642131aecda1d2aa8baa4bd65736606b
Eason Lin <easonlin404@gmail.com>
2019-06-24 17:24:29 +0800

    fix fmt lint

--- example/go-module-support/main.go
+++ example/go-module-support/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/swaggo/examples/go-module-support/api"  // included package from external 
+	"github.com/swaggo/examples/go-module-support/api" // included package from external
 )
 
 // @title Swagger Example API
