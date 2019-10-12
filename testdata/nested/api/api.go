63d8e7beb7e59e103091e368dc05db57717b80d1
MIKI Takushi <takushi-m@users.noreply.github.com>
2019-10-12 14:34:03 +0900

    fix indent

--- testdata/nested/api/api.go
+++ testdata/nested/api/api.go
@@ -8,7 +8,7 @@ import (
 type Foo struct {
 	Field1      string `validate:"required"`
 	OutsideData *nested2.Body
-  InsideData  Bar `validate:"required"`
+	InsideData  Bar `validate:"required"`
 	ArrayField1 []string `validate:"required"`
 	ArrayField2 []Bar `validate:"required"`
 }
