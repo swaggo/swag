008c37baa77bf879a819e7a8eea45a487ec0a178
Bogdan U <ubogdan@gmail.com>
2019-10-14 18:08:07 +0300

    chore(refactor): improve code quality (#540)

--- cmd/swag/main.go
+++ cmd/swag/main.go
@@ -10,13 +10,71 @@ import (
 	"github.com/urfave/cli"
 )
 
-const searchDirFlag = "dir"
-const generalInfoFlag = "generalInfo"
-const propertyStrategyFlag = "propertyStrategy"
-const outputFlag = "output"
-const parseVendorFlag = "parseVendor"
-const parseDependency = "parseDependency"
-const markdownFilesDirFlag = "markdownFiles"
+const (
+	searchDirFlag        = "dir"
+	generalInfoFlag      = "generalInfo"
+	propertyStrategyFlag = "propertyStrategy"
+	outputFlag           = "output"
+	parseVendorFlag      = "parseVendor"
+	parseDependencyFlag  = "parseDependency"
+	markdownFilesFlag    = "markdownFiles"
+)
+
+var initFlags = []cli.Flag{
+	cli.StringFlag{
+		Name:  generalInfoFlag + ", g",
+		Value: "main.go",
+		Usage: "Go file path in which 'swagger general API Info' is written",
+	},
+	cli.StringFlag{
+		Name:  searchDirFlag + ", d",
+		Value: "./",
+		Usage: "Directory you want to parse",
+	},
+	cli.StringFlag{
+		Name:  propertyStrategyFlag + ", p",
+		Value: "camelcase",
+		Usage: "Property Naming Strategy like snakecase,camelcase,pascalcase",
+	},
+	cli.StringFlag{
+		Name:  outputFlag + ", o",
+		Value: "./docs",
+		Usage: "Output directory for al the generated files(swagger.json, swagger.yaml and doc.go)",
+	},
+	cli.BoolFlag{
+		Name:  parseVendorFlag,
+		Usage: "Parse go files in 'vendor' folder, disabled by default",
+	},
+	cli.BoolFlag{
+		Name:  parseDependencyFlag,
+		Usage: "Parse go files in outside dependency folder, disabled by default",
+	},
+	cli.StringFlag{
+		Name:  markdownFilesFlag + ", md",
+		Value: "",
+		Usage: "Parse folder containing markdown files to use as description, disabled by default",
+	},
+}
+
+func initAction(c *cli.Context) error {
+	strategy := c.String(propertyStrategyFlag)
+
+	switch strategy {
+	case swag.CamelCase, swag.SnakeCase, swag.PascalCase:
+	default:
+		return fmt.Errorf("not supported %s propertyStrategy", strategy)
+	}
+
+	return gen.New().Build(&gen.Config{
+		SearchDir:          c.String(searchDirFlag),
+		MainAPIFile:        c.String(generalInfoFlag),
+		PropNamingStrategy: strategy,
+		OutputDir:          c.String(outputFlag),
+		ParseVendor:        c.Bool(parseVendorFlag),
+		ParseDependency:    c.Bool(parseDependencyFlag),
+		MarkdownFilesDir:   c.String(markdownFilesFlag),
+	})
+}
 
 func main() {
 	app := cli.NewApp()
@@ -27,69 +85,10 @@ func main() {
 			Name:    "init",
 			Aliases: []string{"i"},
 			Usage:   "Create docs.go",
-			Action: func(c *cli.Context) error {
-				searchDir := c.String(searchDirFlag)
-				mainAPIFile := c.String(generalInfoFlag)
-				strategy := c.String(propertyStrategyFlag)
-				outputDir := c.String(outputFlag)
-				parseVendor := c.Bool(parseVendorFlag)
-				parseDependency := c.Bool(parseDependency)
-				markdownFilesDir := c.String(markdownFilesDirFlag)
-
-				switch strategy {
-				case swag.CamelCase, swag.SnakeCase, swag.PascalCase:
-				default:
-					return fmt.Errorf("not supported %s propertyStrategy", strategy)
-				}
-
-				return gen.New().Build(&gen.Config{
-					SearchDir:          searchDir,
-					MainAPIFile:        mainAPIFile,
-					PropNamingStrategy: strategy,
-					OutputDir:          outputDir,
-					ParseVendor:        parseVendor,
-					ParseDependency:    parseDependency,
-					MarkdownFilesDir:   markdownFilesDir,
-				})
-			},
-			Flags: []cli.Flag{
-				cli.StringFlag{
-					Name:  "generalInfo, g",
-					Value: "main.go",
-					Usage: "Go file path in which 'swagger general API Info' is written",
-				},
-				cli.StringFlag{
-					Name:  "dir, d",
-					Value: "./",
-					Usage: "Directory you want to parse",
-				},
-				cli.StringFlag{
-					Name:  "propertyStrategy, p",
-					Value: "camelcase",
-					Usage: "Property Naming Strategy like snakecase,camelcase,pascalcase",
-				},
-				cli.StringFlag{
-					Name:  "output, o",
-					Value: "./docs",
-					Usage: "Output directory for al the generated files(swagger.json, swagger.yaml and doc.go)",
-				},
-				cli.BoolFlag{
-					Name:  "parseVendor",
-					Usage: "Parse go files in 'vendor' folder, disabled by default",
-				},
-				cli.BoolFlag{
-					Name:  "parseDependency",
-					Usage: "Parse go files in outside dependency folder, disabled by default",
-				},
-				cli.StringFlag{
-					Name:  "markdownFiles, md",
-					Value: "",
-					Usage: "Parse folder containing markdown files to use as description, disabled by default",
-				},
-			},
+			Action:  initAction,
+			Flags:   initFlags,
 		},
 	}
-
 	err := app.Run(os.Args)
 	if err != nil {
 		log.Fatal(err)
