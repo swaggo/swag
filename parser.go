b734f40071cdd9808551168499c525f0d4402b40
Smotrov Dmitriy <dsxack@gmail.com>
2019-12-04 02:43:07 +0300

    fix issue with import aliases (#483) (#573)

--- parser.go
+++ parser.go
@@ -46,6 +46,9 @@ type Parser struct {
 	// TypeDefinitions is a map that stores [package name][type name][*ast.TypeSpec]
 	TypeDefinitions map[string]map[string]*ast.TypeSpec
 
+	// ImportAliases is map that stores [import name][import package name][*ast.ImportSpec]
+	ImportAliases map[string]map[string]*ast.ImportSpec
+
 	// CustomPrimitiveTypes is a map that stores custom primitive types to actual golang types [type name][string]
 	CustomPrimitiveTypes map[string]string
 
@@ -85,6 +88,7 @@ func New(options ...func(*Parser)) *Parser {
 		},
 		files:                make(map[string]*ast.File),
 		TypeDefinitions:      make(map[string]map[string]*ast.TypeSpec),
+		ImportAliases:        make(map[string]map[string]*ast.ImportSpec),
 		CustomPrimitiveTypes: make(map[string]string),
 		registerTypes:        make(map[string]*ast.TypeSpec),
 	}
@@ -520,6 +524,23 @@ func (parser *Parser) ParseType(astFile *ast.File) {
 				}
 			}
 		}
+	}
+
+	for _, importSpec := range astFile.Imports {
+		if importSpec.Name == nil {
+			continue
+		}
+
+		alias := importSpec.Name.Name
+
+		if _, ok := parser.ImportAliases[alias]; !ok {
+			parser.ImportAliases[alias] = make(map[string]*ast.ImportSpec)
+		}
+
+		importParts := strings.Split(strings.Trim(importSpec.Path.Value, "\""), "/")
+		importPkgName := importParts[len(importParts)-1]
+
+		parser.ImportAliases[alias][importPkgName] = importSpec
 	}
 }
 
---
+++ testdata/alias_import/data/applicationresponse.go
@@ -0,0 +1,11 @@
+package data
+
+import (
+	typesapplication "github.com/swaggo/swag/testdata/alias_import/types"
+)
+
+type ApplicationResponse struct {
+	Application      typesapplication.Application   `json:"application"`
+	ApplicationArray []typesapplication.Application `json:"application_array"`
+	ApplicationTime  typesapplication.DateOnly      `json:"application_time"`
+}
