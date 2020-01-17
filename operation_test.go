c620ba78f80423cbcf10b4f2c1dbae88b49f6e30
easonlin404 <easonlin404@gmail.com>
2020-01-17 17:53:23 +0800

    change external pkg

--- operation_test.go
+++ operation_test.go
@@ -871,7 +871,7 @@ func TestFindTypeDefCoreLib(t *testing.T) {
 }
 
 func TestFindTypeDefExternalPkg(t *testing.T) {
-	spec, err := findTypeDef("github.com/stretchr/testify/assert", "Assertions")
+	spec, err := findTypeDef("github.com/KyleBanks/depth", "Tree")
 	assert.NoError(t, err)
 	assert.NotNil(t, spec)
 }
