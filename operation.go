16c21c4daa9721311c8cdc1f04e759e54ff27751
sdghchj <sdghchj@qq.com>
2020-05-29 14:15:34 +0800

    fix code checking  error  and  fix  #717
    fix code checking  error  and  fix  #717

--- operation.go
+++ operation.go
@@ -210,7 +210,7 @@ func (operation *Operation) ParseParamComment(commentLine string, astFile *ast.F
 				return false
 			}
 			orderedNames := make([]string, 0, len(schema.Properties))
-			for k, _ := range schema.Properties {
+			for k = range schema.Properties {
 				orderedNames = append(orderedNames, k)
 			}
 			sort.Strings(orderedNames)
@@ -239,6 +239,11 @@ func (operation *Operation) ParseParamComment(commentLine string, astFile *ast.F
 					Println(fmt.Sprintf("skip field [%s] in %s is not supported type for %s", name, refType, paramType))
 					continue
 				}
+				param.Nullable = prop.Nullable
+				param.Format = prop.Format
+				param.Default = prop.Default
+				param.Example = prop.Example
+				param.Extensions = prop.Extensions
 				param.CommonValidations.Maximum = prop.Maximum
 				param.CommonValidations.Minimum = prop.Minimum
 				param.CommonValidations.ExclusiveMaximum = prop.ExclusiveMaximum
