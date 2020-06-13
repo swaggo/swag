3706c32f2eaf689d5fb364269473eab089df21e9
chengjin <sdghchj@qq.com>
2020-06-13 22:50:32 +0800

    fix lint check

--- types.go
+++ types.go
@@ -5,12 +5,14 @@ import (
 	"go/ast"
 )
 
+//Schema parsed schema
 type Schema struct {
 	PkgPath      string //package import path used to rename Name of a definition int case of conflict
 	Name         string //Name in definitions
 	*spec.Schema        //
 }
 
+//TypeSpecDef the whole information of a typeSpec
 type TypeSpecDef struct {
 	//path of package starting from under ${GOPATH}/src or from module path in go.mod
 	PkgPath string
@@ -22,17 +24,25 @@ type TypeSpecDef struct {
 	TypeSpec *ast.TypeSpec
 }
 
+//Name name of the typeSpec
 func (t *TypeSpecDef) Name() string {
 	return t.TypeSpec.Name.Name
 }
 
+//FullName full name of the typeSpec
 func (t *TypeSpecDef) FullName() string {
 	return fullTypeName(t.File.Name.Name, t.TypeSpec.Name.Name)
 }
 
+//AstFileInfo information of a ast.File
 type AstFileInfo struct {
-	File        *ast.File
-	Path        string
+	//File ast.File
+	File *ast.File
+
+	//Path path of the ast.File
+	Path string
+
+	//PackagePath package import path of the ast.File
 	PackagePath string
 }
 
