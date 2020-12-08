swaggo/swag

commit 09f9621f095995bdf1a6540f4676103585579fe8

feature: support default response, use comma-separated codes to add a… (#837)
* feature: support default response, use comma-separated codes to add a header for multi responses in one line, use keyword 'all' to add a header for all responses

* update readme_zh-CN.md

* fix tests

* fix tests coverage

operation.go
@@ -111,7 +111,7 @@ func (operation *Operation) ParseComment(comment string, astFile *ast.File) erro
 		err = operation.ParseProduceComment(lineRemainder)
 	case "@param":
 		err = operation.ParseParamComment(lineRemainder, astFile)
-	case "@success", "@failure":
+	case "@success", "@failure", "@response":
 		err = operation.ParseResponseComment(lineRemainder, astFile)
 	case "@header":
 		err = operation.ParseResponseHeaderComment(lineRemainder, astFile)
@@ -611,7 +611,7 @@ func findTypeDef(importPath, typeName string) (*ast.TypeSpec, error) {
 	return nil, fmt.Errorf("type spec not found")
 }
 
-var responsePattern = regexp.MustCompile(`([\d]+)[\s]+([\w\{\}]+)[\s]+([\w\-\.\/\{\}=,\[\]]+)[^"]*(.*)?`)
+var responsePattern = regexp.MustCompile(`([\w,]+)[\s]+([\w\{\}]+)[\s]+([\w\-\.\/\{\}=,\[\]]+)[^"]*(.*)?`)
 
 //RepsonseType{data1=Type1,data2=Type2}
 var combinedPattern = regexp.MustCompile(`^([\w\-\.\/\[\]]+)\{(.*)\}$`)
@@ -743,13 +743,7 @@ func (operation *Operation) ParseResponseComment(commentLine string, astFile *as
 		return err
 	}
 
-	code, _ := strconv.Atoi(matches[1])
-
 	responseDescription := strings.Trim(matches[4], "\"")
-	if responseDescription == "" {
-		responseDescription = http.StatusText(code)
-	}
-
 	schemaType := strings.Trim(matches[2], "{}")
 	refType := matches[3]
 	schema, err := operation.parseAPIObjectSchema(schemaType, refType, astFile)
@@ -757,17 +751,23 @@ func (operation *Operation) ParseResponseComment(commentLine string, astFile *as
 		return err
 	}
 
-	if operation.Responses == nil {
-		operation.Responses = &spec.Responses{
-			ResponsesProps: spec.ResponsesProps{
-				StatusCodeResponses: make(map[int]spec.Response),
-			},
+	for _, codeStr := range strings.Split(matches[1], ",") {
+		if strings.EqualFold(codeStr, "default") {
+			operation.DefaultResponse().Schema = schema
+			operation.DefaultResponse().Description = responseDescription
+		} else if code, err := strconv.Atoi(codeStr); err == nil {
+			if responseDescription == "" {
+				responseDescription = http.StatusText(code)
+			}
+
+			operation.AddResponse(code, &spec.Response{
+				ResponseProps: spec.ResponseProps{Schema: schema, Description: responseDescription},
+			})
+		} else {
+			return fmt.Errorf("can not parse response comment \"%s\"", commentLine)
 		}
 	}
 
-	operation.Responses.StatusCodeResponses[code] = spec.Response{
-		ResponseProps: spec.ResponseProps{Schema: schema, Description: responseDescription},
-	}
 	return nil
 }
 
@@ -779,45 +779,60 @@ func (operation *Operation) ParseResponseHeaderComment(commentLine string, astFi
 		return fmt.Errorf("can not parse response comment \"%s\"", commentLine)
 	}
 
-	response := spec.Response{}
-
-	code, _ := strconv.Atoi(matches[1])
-
-	responseDescription := strings.Trim(matches[4], "\"")
-	if responseDescription == "" {
-		responseDescription = http.StatusText(code)
-	}
-	response.Description = responseDescription
-
 	schemaType := strings.Trim(matches[2], "{}")
-	refType := matches[3]
+	headerKey := matches[3]
+	description := strings.Trim(matches[4], "\"")
+	header := spec.Header{}
+	header.Description = description
+	header.Type = schemaType
 
-	if operation.Responses == nil {
-		operation.Responses = &spec.Responses{
-			ResponsesProps: spec.ResponsesProps{
-				StatusCodeResponses: make(map[int]spec.Response),
-			},
+	if strings.EqualFold(matches[1], "all") {
+		if operation.Responses.Default != nil {
+			if operation.Responses.Default.Headers == nil {
+				operation.Responses.Default.Headers = make(map[string]spec.Header)
+			}
+			operation.Responses.Default.Headers[headerKey] = header
 		}
+		if operation.Responses != nil && operation.Responses.StatusCodeResponses != nil {
+			for code, response := range operation.Responses.StatusCodeResponses {
+				if response.Headers == nil {
+					response.Headers = make(map[string]spec.Header)
+				}
+				response.Headers[headerKey] = header
+				operation.Responses.StatusCodeResponses[code] = response
+			}
+		}
+		return nil
 	}
 
-	response, responseExist := operation.Responses.StatusCodeResponses[code]
-	if responseExist {
-		header := spec.Header{}
-		header.Description = responseDescription
-		header.Type = schemaType
+	for _, codeStr := range strings.Split(matches[1], ",") {
+		if strings.EqualFold(codeStr, "default") {
+			if operation.Responses.Default != nil {
+				if operation.Responses.Default.Headers == nil {
+					operation.Responses.Default.Headers = make(map[string]spec.Header)
+				}
+				operation.Responses.Default.Headers[headerKey] = header
+			}
+		} else if code, err := strconv.Atoi(codeStr); err == nil {
+			if operation.Responses != nil && operation.Responses.StatusCodeResponses != nil {
+				if response, responseExist := operation.Responses.StatusCodeResponses[code]; responseExist {
+					if response.Headers == nil {
+						response.Headers = make(map[string]spec.Header)
+					}
+					response.Headers[headerKey] = header
 
-		if response.Headers == nil {
-			response.Headers = make(map[string]spec.Header)
+					operation.Responses.StatusCodeResponses[code] = response
+				}
+			}
+		} else {
+			return fmt.Errorf("can not parse response comment \"%s\"", commentLine)
 		}
-		response.Headers[refType] = header
-
-		operation.Responses.StatusCodeResponses[code] = response
 	}
 
 	return nil
 }
 
-var emptyResponsePattern = regexp.MustCompile(`([\d]+)[\s]+"(.*)"`)
+var emptyResponsePattern = regexp.MustCompile(`([\w,]+)[\s]+"(.*)"`)
 
 // ParseEmptyResponseComment parse only comment out status code and description,eg: @Success 200 "it's ok"
 func (operation *Operation) ParseEmptyResponseComment(commentLine string) error {
@@ -827,33 +842,49 @@ func (operation *Operation) ParseEmptyResponseComment(commentLine string) error 
 		return fmt.Errorf("can not parse response comment \"%s\"", commentLine)
 	}
 
-	response := spec.Response{}
-
-	code, _ := strconv.Atoi(matches[1])
-
-	response.Description = strings.Trim(matches[2], "")
-
-	if operation.Responses == nil {
-		operation.Responses = &spec.Responses{
-			ResponsesProps: spec.ResponsesProps{
-				StatusCodeResponses: make(map[int]spec.Response),
-			},
+	responseDescription := strings.Trim(matches[2], "\"")
+	for _, codeStr := range strings.Split(matches[1], ",") {
+		if strings.EqualFold(codeStr, "default") {
+			operation.DefaultResponse().Description = responseDescription
+		} else if code, err := strconv.Atoi(codeStr); err == nil {
+			var response spec.Response
+			response.Description = responseDescription
+			operation.AddResponse(code, &response)
+		} else {
+			return fmt.Errorf("can not parse response comment \"%s\"", commentLine)
 		}
 	}
-
-	operation.Responses.StatusCodeResponses[code] = response
 
 	return nil
 }
 
 //ParseEmptyResponseOnly parse only comment out status code ,eg: @Success 200
 func (operation *Operation) ParseEmptyResponseOnly(commentLine string) error {
-	response := spec.Response{}
-
-	code, err := strconv.Atoi(commentLine)
-	if err != nil {
-		return fmt.Errorf("can not parse response comment \"%s\"", commentLine)
+	for _, codeStr := range strings.Split(commentLine, ",") {
+		if strings.EqualFold(codeStr, "default") {
+			_ = operation.DefaultResponse()
+		} else if code, err := strconv.Atoi(codeStr); err == nil {
+			var response spec.Response
+			//response.Description = http.StatusText(code)
+			operation.AddResponse(code, &response)
+		} else {
+			return fmt.Errorf("can not parse response comment \"%s\"", commentLine)
+		}
 	}
+
+	return nil
+}
+
+//DefaultResponse return the default response member pointer
+func (operation *Operation) DefaultResponse() *spec.Response {
+	if operation.Responses.Default == nil {
+		operation.Responses.Default = &spec.Response{}
+	}
+	return operation.Responses.Default
+}
+
+//AddResponse add a response for a code
+func (operation *Operation) AddResponse(code int, response *spec.Response) {
 	if operation.Responses == nil {
 		operation.Responses = &spec.Responses{
 			ResponsesProps: spec.ResponsesProps{
@@ -861,10 +892,7 @@ func (operation *Operation) ParseEmptyResponseOnly(commentLine string) error {
 			},
 		}
 	}
-
-	operation.Responses.StatusCodeResponses[code] = response
-
-	return nil
+	operation.Responses.StatusCodeResponses[code] = *response
 }
 
 // createParameter returns swagger spec.Parameter for gived  paramType, description, paramName, schemaType, required

operation_test.go
@@ -671,6 +671,38 @@ func TestParseResponseCommentWithBasicType(t *testing.T) {
 	assert.Equal(t, expected, string(b))
 }
 
+func TestParseResponseCommentWithBasicTypeAndCodes(t *testing.T) {
+	comment := `@Success 200,201,default {string} string "it's ok'"`
+	operation := NewOperation(nil)
+	err := operation.ParseComment(comment, nil)
+	assert.NoError(t, err, "ParseComment should not fail")
+	b, _ := json.MarshalIndent(operation, "", "    ")
+
+	expected := `{
+    "responses": {
+        "200": {
+            "description": "it's ok'",
+            "schema": {
+                "type": "string"
+            }
+        },
+        "201": {
+            "description": "it's ok'",
+            "schema": {
+                "type": "string"
+            }
+        },
+        "default": {
+            "description": "it's ok'",
+            "schema": {
+                "type": "string"
+            }
+        }
+    }
+}`
+	assert.Equal(t, expected, string(b))
+}
+
 func TestParseEmptyResponseComment(t *testing.T) {
 	comment := `@Success 200 "it's ok"`
 	operation := NewOperation(nil)
@@ -682,6 +714,30 @@ func TestParseEmptyResponseComment(t *testing.T) {
 	expected := `{
     "responses": {
         "200": {
+            "description": "it's ok"
+        }
+    }
+}`
+	assert.Equal(t, expected, string(b))
+}
+
+func TestParseEmptyResponseCommentWithCodes(t *testing.T) {
+	comment := `@Success 200,201,default "it's ok"`
+	operation := NewOperation(nil)
+	err := operation.ParseComment(comment, nil)
+	assert.NoError(t, err, "ParseComment should not fail")
+
+	b, _ := json.MarshalIndent(operation, "", "    ")
+
+	expected := `{
+    "responses": {
+        "200": {
+            "description": "it's ok"
+        },
+        "201": {
+            "description": "it's ok"
+        },
+        "default": {
             "description": "it's ok"
         }
     }
@@ -720,7 +776,74 @@ func TestParseResponseCommentWithHeader(t *testing.T) {
 	comment = `@Header 200 "Mallformed"`
 	err = operation.ParseComment(comment, nil)
 	assert.Error(t, err, "ParseComment should not fail")
+}
 
+func TestParseResponseCommentWithHeaderForCodes(t *testing.T) {
+	operation := NewOperation(nil)
+
+	comment := `@Success 200,201,default "it's ok"`
+	err := operation.ParseComment(comment, nil)
+	assert.NoError(t, err, "ParseComment should not fail")
+
+	comment = `@Header 200,201,default {string} Token "qwerty"`
+	err = operation.ParseComment(comment, nil)
+	assert.NoError(t, err, "ParseComment should not fail")
+
+	comment = `@Header all {string} Token2 "qwerty"`
+	err = operation.ParseComment(comment, nil)
+	assert.NoError(t, err, "ParseComment should not fail")
+
+	b, err := json.MarshalIndent(operation, "", "    ")
+	assert.NoError(t, err)
+
+	expected := `{
+    "responses": {
+        "200": {
+            "description": "it's ok",
+            "headers": {
+                "Token": {
+                    "type": "string",
+                    "description": "qwerty"
+                },
+                "Token2": {
+                    "type": "string",
+                    "description": "qwerty"
+                }
+            }
+        },
+        "201": {
+            "description": "it's ok",
+            "headers": {
+                "Token": {
+                    "type": "string",
+                    "description": "qwerty"
+                },
+                "Token2": {
+                    "type": "string",
+                    "description": "qwerty"
+                }
+            }
+        },
+        "default": {
+            "description": "it's ok",
+            "headers": {
+                "Token": {
+                    "type": "string",
+                    "description": "qwerty"
+                },
+                "Token2": {
+                    "type": "string",
+                    "description": "qwerty"
+                }
+            }
+        }
+    }
+}`
+	assert.Equal(t, expected, string(b))
+
+	comment = `@Header 200 "Mallformed"`
+	err = operation.ParseComment(comment, nil)
+	assert.Error(t, err, "ParseComment should not fail")
 }
 
 func TestParseEmptyResponseOnlyCode(t *testing.T) {
@@ -741,12 +864,44 @@ func TestParseEmptyResponseOnlyCode(t *testing.T) {
 	assert.Equal(t, expected, string(b))
 }
 
+func TestParseEmptyResponseOnlyCodes(t *testing.T) {
+	comment := `@Success 200,201,default`
+	operation := NewOperation(nil)
+	err := operation.ParseComment(comment, nil)
+	assert.NoError(t, err, "ParseComment should not fail")
+
+	b, _ := json.MarshalIndent(operation, "", "    ")
+
+	expected := `{
+    "responses": {
+        "200": {
+            "description": ""
+        },
+        "201": {
+            "description": ""
+        },
+        "default": {
+            "description": ""
+        }
+    }
+}`
+	assert.Equal(t, expected, string(b))
+}
+
 func TestParseResponseCommentParamMissing(t *testing.T) {
 	operation := NewOperation(nil)
 
-	paramLenErrComment := `@Success notIntCode {string}`
+	paramLenErrComment := `@Success notIntCode`
 	paramLenErr := operation.ParseComment(paramLenErrComment, nil)
-	assert.EqualError(t, paramLenErr, `can not parse response comment "notIntCode {string}"`)
+	assert.EqualError(t, paramLenErr, `can not parse response comment "notIntCode"`)
+
+	paramLenErrComment = `@Success notIntCode {string} string "it ok"`
+	paramLenErr = operation.ParseComment(paramLenErrComment, nil)
+	assert.EqualError(t, paramLenErr, `can not parse response comment "notIntCode {string} string "it ok""`)
+
+	paramLenErrComment = `@Success notIntCode "it ok"`
+	paramLenErr = operation.ParseComment(paramLenErrComment, nil)
+	assert.EqualError(t, paramLenErr, `can not parse response comment "notIntCode "it ok""`)
 }
 
 // Test ParseParamComment

go.mod
@@ -6,7 +6,6 @@ require (
 	github.com/ghodss/yaml v1.0.0
 	github.com/gin-gonic/gin v1.6.3
 	github.com/go-openapi/spec v0.19.14
-	github.com/go-openapi/swag v0.19.11 // indirect
 	github.com/go-playground/validator/v10 v10.4.1 // indirect
 	github.com/gofrs/uuid v3.3.0+incompatible
 	github.com/golang/protobuf v1.4.3 // indirect

parser.go
@@ -410,7 +410,7 @@ func isGeneralAPIComment(comment *ast.CommentGroup) bool {
 		attribute := strings.ToLower(strings.Split(commentLine, " ")[0])
 		switch attribute {
 		// The @summary, @router, @success,@failure  annotation belongs to Operation
-		case "@summary", "@router", "@success", "@failure":
+		case "@summary", "@router", "@success", "@failure", "@response":
 			return false
 		}
 	}

commit 64db275ade71e40d71486fe87af5fa203d2fe525

change the regular expression to fix the issue #853 caused by PR #837

operation.go
@@ -603,9 +603,9 @@ func findTypeDef(importPath, typeName string) (*ast.TypeSpec, error) {
 	return nil, fmt.Errorf("type spec not found")
 }
 
-var responsePattern = regexp.MustCompile(`([\w,]+)[\s]+([\w\{\}]+)[\s]+([\w\-\.\/\{\}=,\[\]]+)[^"]*(.*)?`)
+var responsePattern = regexp.MustCompile(`([\w,]+)[\s]+([\w\{\}]+)[\s]+([\w\-\.\/\{\}=,\[\]]+)[\s]+(.*)?`)
 
-//RepsonseType{data1=Type1,data2=Type2}
+//ResponseType{data1=Type1,data2=Type2}
 var combinedPattern = regexp.MustCompile(`^([\w\-\.\/\[\]]+)\{(.*)\}$`)
 
 func (operation *Operation) parseObjectSchema(refType string, astFile *ast.File) (*spec.Schema, error) {

operation_test.go
@@ -672,7 +672,7 @@ func TestParseResponseCommentWithBasicType(t *testing.T) {
 }
 
 func TestParseResponseCommentWithBasicTypeAndCodes(t *testing.T) {
-	comment := `@Success 200,201,default {string} string "it's ok'"`
+	comment := `@Success 200,201,default {string} string "it's ok"`
 	operation := NewOperation(nil)
 	err := operation.ParseComment(comment, nil)
 	assert.NoError(t, err, "ParseComment should not fail")
@@ -681,19 +681,19 @@ func TestParseResponseCommentWithBasicTypeAndCodes(t *testing.T) {
 	expected := `{
     "responses": {
         "200": {
-            "description": "it's ok'",
+            "description": "it's ok",
             "schema": {
                 "type": "string"
             }
         },
         "201": {
-            "description": "it's ok'",
+            "description": "it's ok",
             "schema": {
                 "type": "string"
             }
         },
         "default": {
-            "description": "it's ok'",
+            "description": "it's ok",
             "schema": {
                 "type": "string"
             }
@@ -704,7 +704,7 @@ func TestParseResponseCommentWithBasicTypeAndCodes(t *testing.T) {
 }
 
 func TestParseEmptyResponseComment(t *testing.T) {
-	comment := `@Success 200 "it's ok"`
+	comment := `@Success 200 "it is ok"`
 	operation := NewOperation(nil)
 	err := operation.ParseComment(comment, nil)
 	assert.NoError(t, err, "ParseComment should not fail")
@@ -714,7 +714,7 @@ func TestParseEmptyResponseComment(t *testing.T) {
 	expected := `{
     "responses": {
         "200": {
-            "description": "it's ok"
+            "description": "it is ok"
         }
     }
 }`
@@ -722,7 +722,7 @@ func TestParseEmptyResponseComment(t *testing.T) {
 }
 
 func TestParseEmptyResponseCommentWithCodes(t *testing.T) {
-	comment := `@Success 200,201,default "it's ok"`
+	comment := `@Success 200,201,default "it is ok"`
 	operation := NewOperation(nil)
 	err := operation.ParseComment(comment, nil)
 	assert.NoError(t, err, "ParseComment should not fail")
@@ -732,13 +732,13 @@ func TestParseEmptyResponseCommentWithCodes(t *testing.T) {
 	expected := `{
     "responses": {
         "200": {
-            "description": "it's ok"
+            "description": "it is ok"
         },
         "201": {
-            "description": "it's ok"
+            "description": "it is ok"
         },
         "default": {
-            "description": "it's ok"
+            "description": "it is ok"
         }
     }
 }`