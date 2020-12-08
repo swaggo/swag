98b8c7075fb179dd85a6db9d6099bfed322dc787
Aswin G <aswinganesh666@gmail.com>
2020-12-08 11:28:02 +0530

    Update examples.go (#832)
    Co-authored-by: Eason Lin <easonlin404@gmail.com>

--- example/celler/controller/examples.go
+++ example/celler/controller/examples.go
@@ -139,3 +139,15 @@ func (c *Controller) AttributeExample(ctx *gin.Context) {
 		ctx.Query("default"),
 	))
 }
+
+// PostExample godoc
+// @Summary post request example
+// @Description post request example
+// @Accept json
+// @Produce plain
+// @Param message body model.Account true "Account Info"
+// @Success 200 {string} string "success"
+// @Failure 500 {string} string "fail"
+// @Router /examples/post [post]
+func (c *Controller) PostExample(ctx *gin.Context) {
+}
