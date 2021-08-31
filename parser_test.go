bc0434960ff3b665a2dcb6898f0ee0e4020313cb
Bogdan Ungureanu <ubogdan@gmail.com>
2021-08-31 23:28:07 +0300

    more coverage

--- parser_test.go
+++ parser_test.go
@@ -60,6 +60,50 @@ func TestSetDebugger(t *testing.T) {
 	assert.Equal(t, p.debug, logger)
 }
 
+func TestParser_ParseDefinition(t *testing.T) {
+	p := New()
+
+	// Parsing existing type
+	definition := &TypeSpecDef{
+		PkgPath: "github.com/swagger/swag",
+		File: &ast.File{
+			Name: &ast.Ident{
+				Name: "swag",
+			},
+		},
+		TypeSpec: &ast.TypeSpec{
+			Name: &ast.Ident{
+				Name: "Test",
+			},
+		},
+	}
+
+	expected := &Schema{}
+	p.parsedSchemas[definition] = expected
+
+	schema, err := p.ParseDefinition(definition)
+	assert.NoError(t, err)
+	assert.Equal(t, expected, schema)
+
+	// Parsing *ast.FuncType
+	definition = &TypeSpecDef{
+		PkgPath: "github.com/swagger/swag/model",
+		File: &ast.File{
+			Name: &ast.Ident{
+				Name: "model",
+			},
+		},
+		TypeSpec: &ast.TypeSpec{
+			Name: &ast.Ident{
+				Name: "Test",
+			},
+			Type: &ast.FuncType{},
+		},
+	}
+	_, err = p.ParseDefinition(definition)
+	assert.Error(t, err)
+}
+
 func TestParser_ParseGeneralApiInfo(t *testing.T) {
 	t.Parallel()
 
