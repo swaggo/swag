98750e6753423b12f0f376ecb61a6abfd752522c
Nuno Santos <eu.nuno@gmail.com>
2021-12-10 16:15:47 +0000

    Remove unnecessary check

--- format/format.go
+++ format/format.go
@@ -1,9 +1,7 @@
 package format
 
 import (
-	"fmt"
 	"log"
-	"os"
 
 	"github.com/swaggo/swag"
 )
@@ -26,10 +24,6 @@ type Config struct {
 }
 
 func (f *Fmt) Build(config *Config) error {
-	if _, err := os.Stat(config.SearchDir); os.IsNotExist(err) {
-		return fmt.Errorf("dir: %s is not exist", config.SearchDir)
-	}
-
 	log.Println("Formating code.... ")
 	formater := swag.NewFormater()
 	if err := formater.FormatAPI(config.SearchDir, config.Excludes, config.MainFile); err != nil {
