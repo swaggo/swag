122a2e26a675afe66fac7f525a87fb942964bad6
thinkhp <thinkhp@126.com>
2023-03-23 20:10:14 +0800

    FIX    generics like type JSON[K,V string] map[K]V (#1509)
    Co-authored-by: hepei <hepei@ushareit.com>

--- testdata/generics_basic/types/string.go
+++ testdata/generics_basic/types/string.go
@@ -14,9 +14,22 @@ type TrippleField[T1 any, T2 any] struct {
 	Value2 T2
 }
 
+type ArrayField[T any] []T
+type MapField[K comparable, V any] map[K]V
+type MapFieldValue struct {
+	S string
+	F float64
+}
+type MapFieldNestedStruct[K comparable] map[K]MapFieldValue
+
 type Hello struct {
-	MyStringField1    Field[*string]                `json:"myStringField1"`
-	MyStringField2    Field[string]                 `json:"myStringField2"`
-	MyArrayField      DoubleField[*string, string]  `json:"myNewField"`
-	MyArrayDepthField TrippleField[*string, string] `json:"myNewArrayField"`
+	MyStringField1       Field[*string]                `json:"myStringField1"`
+	MyStringField2       Field[string]                 `json:"myStringField2"`
+	MyArrayField         DoubleField[*string, string]  `json:"myNewField"`
+	MyArrayDepthField    TrippleField[*string, string] `json:"myNewArrayField"`
+	ArrayField           ArrayField[string]            `json:"arrayField"`
+	MapField             MapField[string, float64]     `json:"mapField"`
+	OriginArrayField     []string                      `json:"originArrayField"`
+	OriginMapField       map[string]float64            `json:"originMapField"`
+	MapFieldNestedStruct MapFieldNestedStruct[string]  `json:"mapFieldNestedStruct"`
 }
