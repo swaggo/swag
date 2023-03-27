c794db330c31c31acd367bf4fd90e1b5fc884f4a
Tobias Theel <tt@fino.digital>
2023-03-27 17:26:27 +0200

    add more tests

--- operationv3_test.go
+++ operationv3_test.go
@@ -326,3 +326,33 @@ func TestParseResponseCommentWithNestedFieldsV3(t *testing.T) {
 	assert.Equal(t, spec.SingleOrArray[string](spec.SingleOrArray[string]{"array"}), schemas["data4"].Spec.Properties["data4"].Spec.Type)
 	assert.Equal(t, spec.SingleOrArray[string](spec.SingleOrArray[string]{"object"}), schemas["data4"].Spec.Type)
 }
+
+func TestParseResponseCommentWithDeepNestedFieldsV3(t *testing.T) {
+	t.Parallel()
+
+	comment := `@Success 200 {object} model.CommonHeader{data1=int,data2=[]int,data3=model.Payload{data1=int,data2=model.DeepPayload},data4=[]model.Payload{data1=[]int,data2=[]model.DeepPayload}} "Error message, if code != 200`
+	operation := NewOperationV3(New())
+
+	operation.parser.addTestType("model.CommonHeader")
+	operation.parser.addTestType("model.Payload")
+	operation.parser.addTestType("model.DeepPayload")
+
+	err := operation.ParseComment(comment, nil)
+	assert.NoError(t, err)
+
+	response := operation.Responses.Spec.Response["200"]
+	assert.Equal(t, `Error message, if code != 200`, response.Spec.Spec.Description)
+
+	allOf := response.Spec.Spec.Content["application/json"].Spec.Schema.Spec.AllOf
+	assert.Equal(t, 4, len(allOf))
+
+	schemas := operation.parser.openAPI.Components.Spec.Schemas
+
+	assert.Equal(t, spec.SingleOrArray[string](spec.SingleOrArray[string]{"integer"}), schemas["data1"].Spec.Properties["data1"].Spec.Type)
+	assert.Equal(t, spec.SingleOrArray[string](spec.SingleOrArray[string]{"object"}), schemas["data1"].Spec.Type)
+
+	assert.Equal(t, spec.SingleOrArray[string](spec.SingleOrArray[string]{"array"}), schemas["data2"].Spec.Properties["data2"].Spec.Type)
+	assert.Equal(t, spec.SingleOrArray[string](spec.SingleOrArray[string]{"integer"}), schemas["data2"].Spec.Properties["data2"].Spec.Items.Schema.Spec.Type)
+	assert.Equal(t, spec.SingleOrArray[string](spec.SingleOrArray[string]{"object"}), schemas["data2"].Spec.Type)
+
+}
