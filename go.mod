6c9d7481e2c9e0dd10824ee5263c54d2ae93c7fa
okhowang <3352585+okhowang@users.noreply.github.com>
2025-11-24 18:04:45 +0800

    perf: add --parseGoPackages flag to speed up parse source (#2101)
    using go modern packages API which can make use of cache and multicore

--- go.mod
+++ go.mod
@@ -9,7 +9,7 @@ require (
 	github.com/urfave/cli/v2 v2.3.0
 	golang.org/x/sync v0.12.0
 	golang.org/x/text v0.23.0
-	golang.org/x/tools v0.21.1-0.20240508182429-e35e4ccd0d2d
+	golang.org/x/tools v0.26.0
 	sigs.k8s.io/yaml v1.3.0
 )
 
@@ -26,8 +26,9 @@ require (
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/russross/blackfriday/v2 v2.0.1 // indirect
 	github.com/shurcooL/sanitized_anchor_name v1.0.0 // indirect
-	golang.org/x/mod v0.17.0 // indirect
+	golang.org/x/mod v0.21.0 // indirect
 	golang.org/x/net v0.38.0 // indirect
+	golang.org/x/sync v0.12.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ parsergopackages.go
@@ -0,0 +1,98 @@
+package swag
+
+import (
+	"go/token"
+	"os"
+	"path/filepath"
+	"slices"
+
+	"golang.org/x/tools/go/packages"
+)
+
+func (parser *Parser) loadPackagesAndDeps(searchDirs []string, absMainAPIFilePath string) error {
+	mode := packages.NeedName | packages.NeedFiles | packages.NeedCompiledGoFiles | packages.NeedImports |
+		packages.NeedTypes | packages.NeedTypesSizes | packages.NeedSyntax | packages.NeedTypesInfo
+	if parser.ParseDependency > 0 {
+		mode |= packages.NeedDeps
+	}
+
+	absDirs := make([]string, 0, len(searchDirs)+1)
+	absDirs = append(absDirs, filepath.Dir(absMainAPIFilePath))
+	for _, dir := range searchDirs {
+		absDir, err := filepath.Abs(dir)
+		if err != nil {
+			return err
+		}
+		// load all subpackages keep the same logic with Parser.getAllGoFileInfo
+		absDirs = append(absDirs, absDir+"/...")
+	}
+
+	fset := token.NewFileSet()
+	pkgs, err := packages.Load(&packages.Config{
+		Mode: mode,
+		Fset: fset,
+	}, absDirs...)
+	if err != nil {
+		return err
+	}
+	for _, pkg := range pkgs {
+		for _, e := range pkg.Errors {
+			return e
+		}
+	}
+
+	err = parser.walkPackages(pkgs, func(pkg *packages.Package) error {
+		parseFlag := ParseFlag(ParseAll)
+		if !slices.Contains(pkgs, pkg) {
+			parseFlag = parser.ParseDependency
+		}
+		for i, file := range pkg.CompiledGoFiles {
+			// TODO handle vendor?
+			fileInfo, err := os.Stat(file)
+			if err != nil {
+				return err
+			}
+			if parser.Skip(file, fileInfo) != nil {
+				continue
+			}
+			if err = parser.packages.CollectAstFile(fset, pkg.PkgPath, file, pkg.Syntax[i], parseFlag); err != nil {
+				return err
+			}
+		}
+		return nil
+	})
+	return err
+}
+
+func (parser *Parser) walkPackages(pkgs []*packages.Package, f func(p *packages.Package) error) error {
+	pkgSeen := make(map[string]struct{})
+	return parser.walkPackagesInternal(pkgs, f, pkgSeen)
+}
+
+func (parser *Parser) walkPackagesInternal(pkgs []*packages.Package, f func(p *packages.Package) error,
+	pkgSeen map[string]struct{}) error {
+	for _, pkg := range pkgs {
+		if parser.skipPackageByPrefix(pkg.PkgPath) {
+			continue
+		}
+		if _, ok := pkgSeen[pkg.PkgPath]; ok {
+			continue
+		}
+		pkgSeen[pkg.PkgPath] = struct{}{}
+
+		if err := f(pkg); err != nil {
+			return err
+		}
+
+		if parser.ParseDependency > 0 {
+			imports := make([]*packages.Package, 0, len(pkg.Imports))
+			for _, dep := range pkg.Imports {
+				imports = append(imports, dep)
+			}
+			if err := parser.walkPackagesInternal(imports, f, pkgSeen); err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
